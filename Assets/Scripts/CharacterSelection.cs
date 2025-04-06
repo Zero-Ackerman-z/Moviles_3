8b2c27b03cdc98591d199f052462ac6cc2e29f9c
Zero-Ackerman-z <delia.huayas@tecsup.edu.pe>
2025-04-06 18:44:48 -0500

    Lab 3 Complete 2/3

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -2,21 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class CharacterSelection : MonoBehaviour
 {
-    public PlayerDataSO[] navesDisponibles;  // Array de naves para elegir
-    public Image naveSeleccionadaImagen;   // Imagen UI donde se muestra la nave seleccionada
-    public Text nombreNaveText;            // Texto para mostrar el nombre de la nave seleccionada
-    public PlayerDataSO playerLifeSO;      // Scriptable Object de vida
+    public PlayerDataSO[] navesDisponibles; 
+    public Image naveSeleccionadaImagen;   
+    public TextMeshProUGUI nombreNaveText;           
+    public PlayerDataSO playerLifeSO;
+    public GameObject panelSelector;
+    public GameManager gameManager;
 
     private PlayerDataSO naveSeleccionada;
-
+    private int currentIndex = 0;
+    private float tapTime;
+    private const float doubleTapDelay = 0.3f;
+    private bool isWaitingForSecondTap = false;
     void Start()
     {
-        SeleccionarNave(0);
+        SeleccionarNave(currentIndex);
     }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isWaitingForSecondTap && (Time.time - tapTime) <= doubleTapDelay)
+            {
+                isWaitingForSecondTap = false;
+                ConfirmarSeleccion();
+            }
+            else
+            {
+                isWaitingForSecondTap = true;
+                tapTime = Time.time;
+            }
+        }
 
+        if (isWaitingForSecondTap && (Time.time - tapTime) > doubleTapDelay)
+        {
+            isWaitingForSecondTap = false;
+            CambiarNave();
+        }
+    }
     public void SeleccionarNave(int index)
     {
         naveSeleccionada = navesDisponibles[index];
@@ -24,5 +50,16 @@ public class CharacterSelection : MonoBehaviour
         nombreNaveText.text = naveSeleccionada.naveName;
         playerLifeSO = naveSeleccionada;
         PlayerPrefs.SetString("NaveSeleccionada", naveSeleccionada.naveName);
+    }
+    void CambiarNave()
+    {
+        currentIndex = (currentIndex + 1) % navesDisponibles.Length;
+        SeleccionarNave(currentIndex);
+    }
+    void ConfirmarSeleccion()
+    {
+        playerLifeSO = naveSeleccionada;
+        panelSelector.SetActive(false);
+        gameManager.IniciarJuego(naveSeleccionada);
     }
 }
